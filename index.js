0e0eb724205fb4037f0b2db43e38e6c97a8caed3
Kartik <kartik@hyperverge.co>
2021-04-18 01:36:17 +0530

    Remove: test log statement

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const appLogger = require('./loggers/appLogger');
 
-const logger = appLogger();
-
-logger.log(logger.LOG_LEVELS.ERROR, 'This is up', 'reqid');
-
 module.exports = {
   appLogger,
 };
