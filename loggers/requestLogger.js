d09f9decae0c901871971466766d3470d29b152a
Kartik <kartik@hyperverge.co>
2021-05-05 14:51:13 +0530

    Update: switch to express-winston for request logging

--- loggers/requestLogger.js
+++ loggers/requestLogger.js
@@ -1,7 +1,8 @@
 const winston = require('winston');
 const { createRequestContext } = require('../lib/requestContext');
+const expressWinston = require('express-winston');
 
-let filename = `/var/log/omnia/requests/omnia_req.log`;
+let filename = `./omnia_req.log`;
 
 const addRequestId = (req, res, next) => {
   const { requestId } = createRequestContext();
@@ -9,29 +10,17 @@ const addRequestId = (req, res, next) => {
   next();
 };
 
-const logRequest = (req, res, next) => {
-    const logger = winston.createLogger({
-        level: 'info',
-        format: winston.format.json(),
-        defaultMeta: { service: 'user-service' },
-        transports: [
-          new winston.transports.File({ filename }),
-        ],
-    });
-    const logObject = {};
-    logObject.id = req.id;
-    logObject.ip = req.ip;
-    logObject.headers = req.headers;
-    logObject.body = req.body;
-    logObject.originalUrl = req.originalUrl;
-    logger.log('info', logObject);
-    next();
-}
-
 const requestLogger = (app, product = 'omnia-req', opts) => {
   filename = opts.filename || `/var/log/omnia/requests/${product}.log`;
   app.use(addRequestId);
-  app.use(logRequest);
+  app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.File({ filename }),
+    ],
+    format: winston.format.combine(
+      winston.format.json()
+    )
+  }));
 };
 
 module.exports = { addRequestId, requestLogger };
