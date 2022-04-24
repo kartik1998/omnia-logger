d71fc73e19f0a0a97d5b4bad26a1f390911d8604
kartik1998 <kartikrawat61@gmail.com>
2022-04-25 01:10:42 +0530

    chore: update requestLogger

--- loggers/requestLogger.js
+++ loggers/requestLogger.js
@@ -7,21 +7,20 @@ let filename = `./omnia_req.log`;
 const addRequestId = (req, res, next) => {
   const { requestId } = createRequestContext();
   req.id = requestId;
+  res.locals._requestId = requestId;
   next();
 };
 
 const requestLogger = (app, product = 'omnia-req', opts) => {
   filename = opts.filename || `/var/log/omnia/requests/${product}.log`;
   app.use(addRequestId);
-  app.use(expressWinston.logger({
-    transports: [
-      new winston.transports.File({ filename }),
-    ],
-    format: winston.format.combine(
-      winston.format.json()
-    ),
-    responseWhitelist: ['body', 'headers']
-  }));
+  app.use(
+    expressWinston.logger({
+      transports: [new winston.transports.File({ filename })],
+      format: winston.format.combine(winston.format.json()),
+      responseWhitelist: ['body', 'headers'],
+    }),
+  );
 };
 
 module.exports = { addRequestId, requestLogger };
